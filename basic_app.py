195f2a92d13bf8649cb78fdbe5cbec73722ffa76
griffindex <69892106+griffindex@users.noreply.github.com>
2022-01-31 18:35:43 -0600

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -87,7 +87,7 @@ def home():
 def user_data():
 	
 	
-	auth_manager.get_access_token(state=None, response=None, check_cache=True)
+	auth_manager.refresh_access_token(session.get('access_token'))
 	sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
